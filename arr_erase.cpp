ca020d3c7505e18cf5b3d848dd2a3808109893ab
miraclefusion <gggustn@gmail.com>
2020-11-11 22:49:42 +0900

    tutorial

---
+++ arr_erase.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#incldue <bits/stdc++.h>
+
+using namespace std;
+
+void insert(int idx,int num,int arr[],int& len)
+{
+	for (int i = len; i > idx;i--)
+	{
+		arr[i+1]=arr[i];		
+	}
+	arr[index]=num;
+	len++;
+	
+}
+void erase(int idx,int arr[],int& len)
+{
+	
+}
+
+
+int main()
+{
+	ios :: sync_with_stdio(0);
+	cin.tie(0);
+	
+		
+}
